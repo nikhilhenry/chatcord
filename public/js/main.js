c6b108dc6d02aaa1d73643d8633ac4ac828b6201
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-06-25 16:56:20 +0530

    get form value

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,16 @@
+// @ts-nocheck
+const chatForm = document.getElementById("chat-form");
+
 // @ts-ignore
 const socket = io();
 
 socket.on("message", (message) => {
   console.log(message);
+});
+
+// message submit
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const msg = e.target.elements.msg.value;
+  console.log(msg);
 });
