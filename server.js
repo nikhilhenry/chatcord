08ab67af253bb591b79a491bbe65ea958ceaa2a0
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-06-25 16:45:30 +0530

    init socket

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // serve static
 app.use(express.static(path.join(__dirname, "public")));
 
+// Run when client connects
+io.on("connection", (socket) => {
+  console.log("New web socket connection");
+});
+
 const PORT = 3000 || process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
