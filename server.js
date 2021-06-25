ba4e9fbe8c41f1cb17c7e5b382fd45830e4d153c
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-06-25 16:41:04 +0530

    serve static

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+// serve static
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = 3000 || process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
