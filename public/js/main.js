038b31936bf67a597c94f2d1e6612b4a2ea7209d
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2021-06-25 18:05:35 +0530

    make it same line

--- public/js/main.js
+++ public/js/main.js
@@ -33,7 +33,7 @@ chatForm.addEventListener("submit", (e) => {
 function outputMessage(message) {
   const div = document.createElement("div");
   div.classList.add("message");
-  div.innerHTML = `<p class="meta">${message.username}</p> <span>${message.time}</span></p>
+  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
   <p class="text">
   ${message.text}
   </p>`;
